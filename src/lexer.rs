55187ec4d9eb6add9ff67830dd43e9be387fc64c
Sam Hood <samjdhood@gmail.com>
2026-09-10 00:10:37 -0500

    initial commit

---
+++ src/lexer.rs
@@ -0,0 +1,98 @@
+pub struct Lexer<'a> {
+    input: &'a [u8],
+    input_len: usize,
+    current_pos: usize,
+}
+
+fn is_whitespace(c: u8) -> bool {
+    // ASCII codes 9 thru 13 and 32 are all 
+    // whitespaces char codes
+    if (9..13).contains(&c) || c == 32 {
+        return true;
+    }
+
+    return false;
+}
+
+impl<'a> Lexer<'a>  {
+    pub fn new(f: &'a [u8]) -> Self {
+        Self {
+            input: f,
+            input_len: f.len(),
+            current_pos: 0,
+        }
+    }
+
+    pub fn get_token(&mut self) -> Option<u8> {
+        if self.current_pos < self.input_len {
+            while (is_whitespace(self.input[self.current_pos])) {
+                self.current_pos += 1;
+            }
+            self.current_pos += 1;
+            return Some(self.input[self.current_pos - 1]);
+        }
+
+        return None;
+    }
+
+    pub fn peek_token(&self) -> Option<u8> {
+        if (self.current_pos < self.input_len) && (self.current_pos + 1) < self.input_len  {
+            return Some(self.input[self.current_pos + 1]);
+        }
+
+        return None;
+    }
+
+    pub fn rewind_input(&mut self) { 
+        self.current_pos = 0;
+    }
+
+    pub fn rewind_input_steps(&mut self, rewind_steps: usize) -> bool {
+        let mut x = rewind_steps;
+    
+        // make sure we aren't trying to rewind more than we have input
+        if (self.current_pos - rewind_steps) < 0 {
+            return false;
+        }
+
+        while (x != 0 && self.current_pos >= 0) {
+            while (is_whitespace(self.input[self.current_pos]) && (self.current_pos >= 0)) {
+                self.current_pos -= 1;
+            }
+            x -= 1;
+            self.current_pos -= 1;
+        }
+
+        return true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_DATA: [u8; 7] = [3, 5, 2, 3, 10, 24, 32];
+
+    #[test]
+    fn test_init() {
+        let test_lex = Lexer::new(&TEST_DATA);
+        assert!(test_lex.input_len == 7);
+        assert!(test_lex.input == TEST_DATA);
+    }
+
+    #[test]
+    fn test_rewind_input() {
+        let mut test_lex = Lexer::new(&TEST_DATA);
+        test_lex.rewind_input();
+        assert!(test_lex.current_pos == 0);
+    }
+
+    // FAILING
+    #[test]
+    fn test_rewind_input_steps() {
+        let mut test_lex = Lexer::new(&TEST_DATA);
+        test_lex.current_pos = 10;
+        assert!((test_lex.rewind_input_steps(8)) && test_lex.current_pos == 2);
+        assert!(!(test_lex.rewind_input_steps(11)));
+    }
+}
